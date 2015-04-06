35ceed56296cb39def59be281e61d53891d32a91
Micah Schaefer <micah@kulo.us>
2015-04-06 12:48:13 -0400

    added link in footer

--- footer.php
+++ footer.php
@@ -12,6 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
